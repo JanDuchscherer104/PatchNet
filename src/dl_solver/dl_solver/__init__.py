2b393ccf8013d9d14c3688c689b960574e3a2796
Jan Duchscherer <duchsche@hm.edu>
2024-04-25 17:04:31 +0200

    mv learnable fourier features to own src

--- src/dl_solver/dl_solver/__init__.py
+++ src/dl_solver/dl_solver/__init__.py
@@ -1,12 +1,9 @@
-from src.dl_solver.lib.learnable_fourier_features.positional_encoding import (
-    LearnableFourierFeatures,
-)
-
 from .config import Config, HyperParameters, PiecemakerConfig
 from .jigsaw_dataset import JigsawDataset
 from .lit_datamodule import LitJigsawDatamodule
 from .lit_module import LitJigsawModule
 from .lit_trainer_factory import TrainerFactory
+from .positional_encoding import LearnableFourierFeatures
 
 __all__ = [
     "Config",
