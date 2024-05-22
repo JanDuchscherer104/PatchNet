583142702231e92ec19e0795ada7aeb65041d7cd
Jan Duchscherer <duchsche@hm.edu>
2024-05-22 12:44:50 +0200

    update: docstrings and param-handling in Patchnet

--- src/dl_solver/dl_solver/patchnet.py
+++ src/dl_solver/dl_solver/patchnet.py
@@ -1,195 +1,188 @@
-from typing import Annotated, Optional, Tuple
+from typing import Optional, Tuple
 
-import numpy as np
 import torch
 import torch.nn as nn
 from torch import Tensor
 from torch.functional import F
 from torchvision.models import EfficientNet_V2_S_Weights, efficientnet_v2_s
 
-from .config import HyperParameters
+from .hparams import HParams
 from .nn_utils import nn_utils
 from .positional_encoding import LearnableFourierFeatures
 
 
 class EfficientNetV2(nn.Module):
-    def __init__(self, num_features_out: int, is_trainable: bool = False, **kwargs):
+    def __init__(self, hparams: HParams.Backbone):
         """
-        TODO: Do not set to non-trainable. Use different learning rates and lr-schedulers for backbone and head.
-        Args:
-            num_features_out: int - Number of output features from the backbone
-            **kwargs:
-                inverted_residual_setting: Sequence[Union[MBConvConfig, FusedMBConvConfig]],
-                dropout: float,
-                stochastic_depth_prob: float = 0.2,
-                num_classes: int = 1000,
-                norm_layer: Optional[Callable[..., nn.Module]] = None,
-                last_channel: Optional[int] = None,
+        EfficientNetV2 backbone with optional trainable parameters.
         """
         super().__init__()
 
         self.backbone = efficientnet_v2_s(
-            weights=EfficientNet_V2_S_Weights.IMAGENET1K_V1, **kwargs
+            weights=EfficientNet_V2_S_Weights.IMAGENET1K_V1
         )
 
         # Replace the classification head
-        # self.classifier = nn.Sequential(
-        #     nn.Dropout(p=dropout, inplace=True),
-        #     nn.Linear(lastconv_output_channels, num_classes),
-        # )
+        assert isinstance(linear := self.backbone.classifier[1], nn.Linear)
         self.backbone.classifier[1] = nn.Linear(
-            self.backbone.classifier[1].in_features, num_features_out
+            linear.in_features, hparams.num_features_out, bias=hparams.has_bias
         )
 
-        # Set the backbone to be non-trainable
-        if not is_trainable:
+        # Set the backbone to be non-trainable if specified
+        if not hparams.is_trainable:
             for param in self.backbone.parameters():
-                param.requires_grad = False
+                param.requires_grad_(False)
         self.backbone.classifier[1].requires_grad_(True)
 
     def forward(self, x: Tensor) -> Tensor:
         """
+        Forward pass for EfficientNetV2.
+
         Args:
-            x: Tensor[torch.float32] - (B, num_pieces, 3, H, W)
+            x (Tensor['B num_pieces 3 H W', float32]): The puzzle pieces
+
         Returns:
-            Tensor[torch.float32] - (B, num_pieces, num_features)
-
-        Extracts features of each puzzle piece independently. And returns a flattened
-        feature tensor for each piece.
+            Tensor['B num_pieces num_features_out', float32]: Features for each puzzle piece
         """
-
-        return torch.stack([self.backbone(x_i) for x_i in x.unbind(dim=1)], dim=1)
+        B, num_pieces, C, H, W = x.shape
+        x = x.view(B * num_pieces, C, H, W)
+        features = self.backbone(x)
+        features = features.view(B, num_pieces, -1)
+        return features
 
 
 class PuzzleTypeClassifier(nn.Module):
-    """
-    For each puzzle piece, predict the type of the piece:
-        - Corner (0) ~ (row_idx == 0 || row_idx == num_rows - 1) && (col_idx == 0 || col_idx == num_cols - 1)
-        - Edge (1) ~ (row_idx == 0 || row_idx == num_rows - 1) || (col_idx == 0 || col_idx == num_cols - 1)
-        - Center (2) ~ else
-    """
+    def __init__(self, hparams: HParams.TypeClassifier):
+        """
+        Classifier for predicting the type of each puzzle piece.
 
-    def __init__(self, input_features: int) -> None:
+        Args:
+            hparams (HyperParameters.Classifier): Hyperparameters for the classifier.
+        """
         super().__init__()
-        self.fc = nn.Linear(input_features, 3)
+        self.fc = nn.Linear(hparams.input_features, 3)
         self.embedding = nn.Sequential(
             nn.Softmax(dim=-1),
-            LearnableFourierFeatures(
-                pos_dim=3, d_dim=input_features, f_dim=128, h_dim=256, g_dim=1
-            ),
+            nn.Linear(3, hparams.fourier_embedding.d_dim, bias=False),
+            # LearnableFourierFeatures(**hparams.fourier_embedding.model_dump()),
         )
 
     def forward(self, x: Tensor) -> Tuple[Tensor, Tensor]:
         """
+        Forward pass for PuzzleTypeClassifier.
+
         Args:
-            x (Tensor): [B, num_pieces, num_features]
+            x (Tensor['B num_pieces num_features_out', float32]):
 
         Returns:
-            Tensor: [B, num_pieces, 3] - [corner, edge, center]
+            Tuple[Tensor, Tensor]:
+                - Logits for each puzzle piece type of shape (Tensor['B num_pieces 3', float32])
+                - Embedded tensor of shape (Tensor['B num_pieces num_features_out', float32])
         """
         x = self.fc(x)
-        return x, self.embedding(x.unsqueeze(-2))
+        return x, self.embedding(x)
 
 
 class Transformer(nn.Module):
-    def __init__(
-        self,
-        d_model: int,
-        num_encoder_layers: int,
-        num_decoder_layers: int,
-        decoder_dim_feedforward: int,
-        nhead: int = 8,
-    ):
+    def __init__(self, hparams: HParams.Transformer):
+        """
+        Transformer model for encoding and decoding puzzle piece features.
+
+        Args:
+            hparams (HyperParameters.Transformer): Hyperparameters for the transformer.
+        """
         super().__init__()
-        # TODO all is args are pretty much arbitrarys: Optimize hyperparameters
-        # - d_model
-        # - dim_feedforward
-        # - nhead
-        # - num_encoder_layers
-        # - norm
-        # TODO: Should we use both Encoder and Decoder? We've only used Decoder so far.
-        # TODO: Find papet that discusses different use cases of Encoder and Decoder
 
         self.encoder = nn.TransformerEncoder(
             nn.TransformerEncoderLayer(
-                d_model=d_model,
-                nhead=nhead,
+                d_model=hparams.d_model,
+                nhead=hparams.nhead,
                 batch_first=True,
-                dim_feedforward=512,
+                dim_feedforward=hparams.dim_feedforward,
                 activation=F.silu,
             ),
-            num_layers=num_encoder_layers,
-            norm=nn.LayerNorm(d_model),
+            num_layers=hparams.num_encoder_layers,
+            norm=nn.LayerNorm(hparams.d_model),
         )
 
-        assert d_model % nhead == 0
         self.decoder = nn.TransformerDecoder(
             nn.TransformerDecoderLayer(
-                d_model=d_model,
-                nhead=nhead,
+                d_model=hparams.d_model,
+                nhead=hparams.nhead,
                 batch_first=True,
-                dim_feedforward=decoder_dim_feedforward,
+                dim_feedforward=hparams.dim_feedforward,
                 activation=F.silu,
             ),
-            num_layers=num_decoder_layers,
-            norm=nn.LayerNorm(d_model),  # TODO: Use GroupNorm instead of LayerNorm
+            num_layers=hparams.num_decoder_layers,
+            norm=nn.LayerNorm(hparams.d_model),
         )
 
     def forward(
-        self,
-        src: Tensor,
-        pos_encoding: Tensor,
-        memory: Optional[Tensor] = None,
+        self, src_seq: Tensor, tgt_seq: Tensor, memory: Optional[Tensor] = None
     ) -> Tuple[Tensor, Tensor]:
         """
-        Args:
-            src: Tensor[torch.float32] - (B, num_pieces, num_features)
-            pos_encoding: Tensor[torch.float32] - (B, num_pieces, num_features) [row_idx, col_idx, rotation]
-            memory: Tensor[torch.float32] - (B, num_pieces, num_features)
-        Returns:
-            Tensor[torch.float32] - (B, num_pieces, num_features)
-            Tensor[torch.float32] - (B, num_pieces, num_features)
-        """
-        encoder_memory = self.encoder(src) if memory is None else memory
+        Forward pass for Transformer.
 
+        Args:
+            src_seq (Tensor['B num_pieces num_features_out', float32]): The sequence of puzzle piece features
+            tgt_seq (Tensor['B num_pieces num_features_out', float32]): The target sequence of position embeddings
+            memory (Tensor['B num_pieces num_features_out', float32], optional):
+
+        Returns:
+            Tuple[Tensor, Tensor]:
+                - Decoder output tensor (Tensor['B num_pieces num_features_out', float32])
+                - Encoder memory tensor (Tensor['B num_pieces num_features_out', float32])
+        """
+        encoder_memory = self.encoder.forward(src_seq) if memory is None else memory
+
+        tgt_mask = None
         if self.training:
-            tgt_mask = nn_utils.generate_causal_mask(
-                pos_encoding.size(1), pos_encoding.device
+            tgt_mask = nn.Transformer.generate_square_subsequent_mask(
+                tgt_seq.size(1), device=tgt_seq.device
             )
 
-        decoder_output = self.decoder(
-            pos_encoding,
-            encoder_memory,
+        decoder_output = self.decoder.forward(
+            tgt=tgt_seq,
+            memory=encoder_memory,
+            tgt_mask=tgt_mask,
             tgt_is_causal=self.training,
-            tgt_mask=tgt_mask if self.training else None,
         )
 
         return decoder_output, encoder_memory
 
 
-class DynamicPuzzleClassifier(nn.Module):
-    def __init__(self, input_features, max_rows: int, max_cols: int):
-        super().__init__()
-        self.max_rows = max_rows
-        self.max_cols = max_cols
+class DynamicIdxClassifier(nn.Module):
+    def __init__(self, hparams: HParams.IdxClassifier):
+        """
+        Classifier for predicting row, column, and rotation logits for each puzzle piece.
 
-        self.fc_rows = nn.Linear(input_features, max_rows)
-        self.fc_cols = nn.Linear(input_features, max_cols)
-        # self.fc_pos = nn.Linear(input_features, max_rows * max_cols) TODO: use
-        self.fc_rot = nn.Linear(input_features, 4)  # For rotations
+        Args:
+            hparams (HyperParameters.Classifier): Hyperparameters for the classifier.
+        """
+        super().__init__()
+        self.max_rows = hparams.max_rows
+        self.max_cols = hparams.max_cols
+
+        self.fc_rows = nn.Linear(hparams.input_features, self.max_rows)
+        self.fc_cols = nn.Linear(hparams.input_features, self.max_cols)
+        self.fc_rot = nn.Linear(hparams.input_features, 4)  # For rotations
 
     def forward(
         self, x: Tensor, actual_rows: int, actual_cols: int
     ) -> Tuple[Tensor, Tensor, Tensor]:
         """
+        Forward pass for DynamicPuzzleClassifier.
+
         Args:
-            x: Tensor[torch.float32] - (B, num_pieces, num_features)
-            actual_rows: int - Number of actual rows in the puzzle
-            actual_cols: int - Number of actual columns in the puzzle
+            x Tensor['B num_pieces num_features_out', float32]:
+            actual_rows (int): Number of actual rows in the puzzle
+            actual_cols (int): Number of actual columns in the puzzle
+
         Returns:
-            row_logits: Tensor[torch.float32] - (B, num_pieces, max_rows)
-            col_logits: Tensor[torch.float32] - (B, num_pieces, max_cols)
-            rotation_logits: Tensor[torch.float32] - (B, num_pieces, 4)
+            Tuple[Tensor, Tensor, Tensor]:
+                - Row logits (Tensor['B num_pieces max_rows', float32])
+                - Column logits (Tensor['B num_pieces max_cols', float32])
+                - Rotation logits (Tensor['B num_pieces 4', float32])
         """
         row_logits = self.fc_rows(x)
         col_logits = self.fc_cols(x)
@@ -198,72 +191,52 @@ class DynamicPuzzleClassifier(nn.Module):
         row_logits[..., actual_rows:].fill_(float("-inf"))
         col_logits[..., actual_cols:].fill_(float("-inf"))
 
-        # Concatenate the tensors along a new dimension
         return row_logits, col_logits, rot_logits
 
 
 class PatchNet(nn.Module):
-    hparams: HyperParameters
-    backbone: EfficientNetV2
-    transformer: Transformer
-    classifier: DynamicPuzzleClassifier
+    def __init__(self, hparams: HParams):
+        """
+        PATCHNet model for solving jigsaw puzzles using a Transformer and CNN hybrid approach.
 
-    def __init__(self, hparams: HyperParameters):
+        Args:
+            hparams (HyperParameters): Hyperparameters for the model.
+        """
         super().__init__()
         self.hparams = hparams
 
-        self.backbone = EfficientNetV2(
-            num_features_out=hparams.num_features_out,
-            is_trainable=hparams.backbone_is_trainable,
-        )
+        self.backbone = EfficientNetV2(hparams.backbone)
+        self.puzzle_type_classifier = PuzzleTypeClassifier(hparams.type_classifier)
+        self.transformer = Transformer(hparams.transformer)
+        self.classifier = DynamicIdxClassifier(hparams.idx_classifier)
 
-        self.transformer = Transformer(
-            d_model=hparams.num_features_out,
-            nhead=8,
-            num_encoder_layers=3,
-            num_decoder_layers=3,
-            decoder_dim_feedforward=512,
-        )
-
-        # TODO: try using a single positional embedding, but with different groups: extend rotational embedding to two dimensions!
         self.spatial_embedding = LearnableFourierFeatures(
-            pos_dim=2, f_dim=128, h_dim=256, d_dim=hparams.num_features_out, g_dim=1
+            **hparams.fourier_embedding_pos.model_dump()
         )
         self.rotation_embedding = LearnableFourierFeatures(
-            pos_dim=1, f_dim=2, h_dim=64, d_dim=hparams.num_features_out, g_dim=1
+            **hparams.fourier_embedding_rot.model_dump()
         )
 
-        # Start and End tokens for the decoder sequence
         self.start_of_seq_token = nn.Parameter(
-            torch.randn(1, 1, hparams.num_features_out, dtype=torch.float32),
+            torch.randn(1, 1, hparams.transformer.d_model, dtype=torch.float32),
             requires_grad=True,
         )
-        self.end_of_seq_token = nn.Parameter(
-            torch.randn(1, 1, hparams.num_features_out, dtype=torch.float32),
-            requires_grad=True,
-        )
-
-        self.classifier = DynamicPuzzleClassifier(
-            input_features=hparams.num_features_out,
-            max_rows=hparams.puzzle_shape[0],  # TODO
-            max_cols=hparams.puzzle_shape[1],
-        )
-
-        self.puzzle_type_classifier = PuzzleTypeClassifier(hparams.num_features_out)
 
     def forward(
         self, x: Tensor, true_pos_seq: Optional[Tensor] = None
     ) -> Tuple[Tensor, Tensor, Tuple[Tensor, Tensor, Tensor]]:
         """
+        Forward pass for PATCHNet.
+
         Args:
-            x: Tensor[torch.float32] - (B, num_pieces, 3, H, W)
+            x (Tensor['B num_pieces 3 H W', float32]): The puzzle pieces
+            true_pos_seq (Tensor['B num_pieces 3', float32], optional): The tgt position sequence containing {row, col, rot} in dim=-1
+
         Returns:
-            puzzle_type_logits: Tensor[torch.float32] - (B, num_pieces, 3) [corner, edge, center]
-            pos_seq: Tensor[torch.float32] - (B, num_pieces, 3) [row_idx, col_idx, rotation]
-            logits: Tuple[Tensor[torch.float32], Tensor[torch.float32], Tensor[torch.float32]]
-                row_logits: Tensor[torch.float32] - (B, num_pieces, max_rows)
-                col_logits: Tensor[torch.float32] - (B, num_pieces, max_cols)
-                rotation_logits: Tensor[torch.float32] - (B, num_pieces, 4)
+            Tuple[Tensor, Tensor, Tuple[Tensor, Tensor, Tensor]]:
+                - Puzzle type logits (Tensor['B num_pieces 3', float32])
+                - Position sequence tensor (Tensor['B num_pieces 3', float32])
+                - Tuple of logits for row, column, and rotation
         """
         x = self.backbone(x)
         puzzle_type_logits, puzzle_type_embedding = self.puzzle_type_classifier.forward(
@@ -271,16 +244,12 @@ class PatchNet(nn.Module):
         )
         x = x + puzzle_type_embedding
 
-        # TODO: Potential Feature Reduction
-        # TODO: create initial positional embedding of shape (num_pieces, 3) [row_idx, col_idx, rotation]
-
         if self.training:
             assert true_pos_seq is not None
             pos_seq = torch.cat(
                 [
                     self.start_of_seq_token.expand(x.size(0), 1, -1),
                     self._embedd_pos_seq(true_pos_seq.clone().to(x)),
-                    self.end_of_seq_token.expand(x.size(0), 1, -1),
                 ],
                 dim=1,
             )
@@ -288,19 +257,31 @@ class PatchNet(nn.Module):
         else:
             pos_seq, logits = self._autoregressive_decode(x)
 
-        def remove_special_tokens(x):
+        def remove_sart_token(x):
             if not self.training:
                 return x
             if isinstance(x, tuple):
-                return tuple(map(remove_special_tokens, x))
-            return x[:, 1:-1, ...]
+                return tuple(map(remove_sart_token, x))
+            return x[:, 1:, ...]
 
-        return (puzzle_type_logits, *remove_special_tokens((pos_seq, logits)))  # type: ignore
+        return (puzzle_type_logits, *remove_sart_token((pos_seq, logits)))  # type: ignore
 
     def _soft_forward_step(
         self, x: Tensor, pos_seq: Tensor, encoder_memory: Optional[Tensor] = None
     ) -> Tuple[Tensor, Tensor]:
+        """
+        Soft forward step for training.
 
+        Args:
+            x (Tensor['B num_pieces num_features_out', float32]):
+            pos_seq (Tensor['B num_pieces num_features_out', float32]):
+            encoder_memory (Tensor['B num_pieces num_features_out', float32], optional):
+
+        Returns:
+            Tuple[Tensor, Tensor]:
+                - Tensor['B num_pieces num_features_out', float32]: Position sequence tensor
+                - Tensor['B num_pieces num_features_out', float32]: Logits tensor
+        """
         x, encoder_memory = self.transformer(x, pos_seq, encoder_memory)
         logits = self.classifier(x, *self.hparams.puzzle_shape)
 
@@ -327,6 +308,17 @@ class PatchNet(nn.Module):
     def _autoregressive_decode(
         self, x: Tensor
     ) -> Tuple[Tensor, Tuple[Tensor, Tensor, Tensor]]:
+        """
+        Autoregressive decoding for inference.
+
+        Args:
+            x (Tensor['B num_pieces num_features_out', float32]):
+
+        Returns:
+            Tuple[Tensor, Tuple[Tensor, Tensor, Tensor]]:
+                - Tensor['B num_pieces num_features_out', float32]: Position sequence tensor
+                - Tuple of logits for row, column, and rotation
+        """
         B, L, _F = x.shape
         pos_seq = torch.cat([self.start_of_seq_token.expand(B, 1, -1)], dim=1)
         encoder_memory = None
